5471090fae40ef907c0eabc92983b718090b486f
Em1lyK <110668675+Em1lyK@users.noreply.github.com>
2023-04-27 09:56:14 +0100

    tidy up 30yr forecast

--- 30yr_projection.R
+++ 30yr_projection.R
@@ -1,10 +1,21 @@
 #forecast band d assuming 3% increase in each year
 
-#define variables
-#band d percentage increase
+#### define variables ####
+
+#band d percentage increase and forecast length
 growth_rate <- 0.03
+forecast_len <- 30
+
+#create multiplication coefficients of ct for the next x years
+power <- (1:forecast_len)
+increase <- as.matrix((1+growth_rate)^power)
+
+#empty data frame
 d <- data.frame(matrix(ncol = 29, nrow = 408))
 
+
+
+#### function to multiply a column by a list of numbers and output many columns ####
 multiply_repeat <- function(b, c, d) {
   i <- 1 
   while (i < 30) {
@@ -18,51 +29,25 @@ multiply_repeat <- function(b, c, d) {
 
 
 
-####inital data ####
+####initial data ####
 
 current_bandd <- ctr %>% 
   select(ecode:class, avebandd_expp2324)
 
-#forecast band d
+#### forecast band d ####
 
+#call forecasting function 
 multiply_repeat(current_bandd$avebandd_expp2324, increase, forecast_bandd)
 
+#################################################################
+#### QA ####
+#################################################################
 
-
-test <- as.data.frame(d$X29/current_bandd$avebandd_expp2324 == 1.03^29)
 
 d <-d %>% 
   mutate(check = X29/current_bandd$avebandd_expp2324 == 1.03^29) %>% 
   mutate(num_check = X29/current_bandd$avebandd_expp2324 - 1.03^29) %>% 
   add_column(current_bandd$authority)
 
-print(current_bandd[62,3])
-print(current_bandd[63,3])
-print(if())
-
-test_df <- data.frame(matrix(ncol = 29, nrow = 408, 1.03^29))
-test_df = 1.03^29
-
-check <- test_df- 
-
-
-forecast_bandd <- current_bandd %>%
-  mutate(yr2425 = avebandd_expp2324*(1+percent_inc)) 
-
-multiply_repeat(current_bandd, current_bandd$avebandd_expp2324, increase, forecast_bandd)
-
-%>% 
-  mutate(yr2526 = yr2425*(1+percent_inc))
-d <- data.frame()
-multiply_repeat(current_bandd$avebandd_expp2324, increase, forecast_bandd)
-
-power <- (1:30)
-increase <- as.matrix(1.03^power)
-bandd2324<- 
-
-
-d[,1] <- data.frame(current_bandd$avebandd_expp2324*increase[[1]])
-d[,2] <- data.frame(current_bandd$avebandd_expp2324*increase[[2]])
-d[,3] <- data.frame(current_bandd$avebandd_expp2324*increase[[3]])
 
        

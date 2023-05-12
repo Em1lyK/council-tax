1039c29df71181753942397e72d6ecbc9f793d33
Em1lyK <110668675+Em1lyK@users.noreply.github.com>
2023-05-12 14:06:09 +0100

    further updates to read in function

--- historic_ctr_V2.r
+++ historic_ctr_V2.r
@@ -26,13 +26,13 @@ ctr_1617_raw <- readxl::read_excel(ctr_1617_loc, sheet = "Data", skip = 3)
 ctr_1516_raw <- readxl::read_excel(ctr_1516_loc, sheet = "Data", skip = 3)
 
 #function to clean ctrs and select relevent columns (21/22 to 22/23)
-clean_ctr <- function(output, raw_input, tstb_py, tstb_cy) {
+clean_ctr <- function(output, raw_input, tstb_cy) {
     a <- raw_input |>
     clean_names()
 
     a <- a |>
-    rename(!! paste0("tstb_", tstb_py) :=  x7_council_tax_base_for_council_tax_setting_purposes, !! paste0("tstb_", tstb_cy) := x22) |>
-    select(ecode:class, paste0("tstb_", tstb_py):paste0("tstb_", tstb_cy), ons_code) |>
+    rename(!! paste0("tstb_", tstb_cy) := x22) |>
+    select(ecode:class, paste0("tstb_", tstb_cy), ons_code) |>
     relocate(ons_code) |>
     filter(ecode != "NA")
     return(assign(output, a, envir = parent.frame()))
@@ -40,26 +40,26 @@ clean_ctr <- function(output, raw_input, tstb_py, tstb_cy) {
 }
 
 #function to clean ctrs and select relevent columns (16/17 to 20/21)
-clean_ctr_2 <- function(output, raw_input, tstb_py, tstb_cy) {    
+clean_ctr_2 <- function(output, raw_input, tstb_cy) {    
     a <- raw_input |>
     clean_names()
 
     a <- a |>
-    rename(!! paste0("tstb_", tstb_py) :=  x7_council_tax_base_for_council_tax_setting_purposes, !! paste0("tstb_", tstb_cy) := x20) |>
-    select(ecode:class, paste0("tstb_", tstb_py):paste0("tstb_", tstb_cy)) |>
+    rename( !! paste0("tstb_", tstb_cy) := x20) |>
+    select(ecode:class, paste0("tstb_", tstb_cy)) |>
     filter(ecode != "NA")
     return(assign(output, a, envir = parent.frame()))
     
 }
 
 #function to clean ctrs and select relevent columns (15/16)
-clean_ctr_3 <- function(output, raw_input, tstb_py, tstb_cy) {
+clean_ctr_3 <- function(output, raw_input, tstb_cy) {
     a <- raw_input |>
         clean_names()
 
     a <- a |>
-    rename(!! paste0("tstb_", tstb_py) :=  x8_council_tax_base_for_council_tax_setting_purposes_line_4_x_line_6_line_7, !! paste0("tstb_", tstb_cy) := x22) |>
-    select(ecode:class, paste0("tstb_", tstb_py):paste0("tstb_", tstb_cy)) |>
+    rename(!! paste0("tstb_", tstb_cy) := x22) |>
+    select(ecode:class, paste0("tstb_", tstb_cy)) |>
     filter(ecode != "NA")
     return(assign(output, a, envir = parent.frame()))
 }
@@ -67,23 +67,23 @@ clean_ctr_3 <- function(output, raw_input, tstb_py, tstb_cy) {
 
 
 #call function 
-clean_ctr("ctr_2223", ctr_2223_raw, "2122", "2223")
+clean_ctr("ctr_2223", ctr_2223_raw, "2223")
 
-clean_ctr("ctr_2122", ctr_2122_raw, "2021", "2122")
+clean_ctr("ctr_2122", ctr_2122_raw, "2122")
 
 #call second function due to change in the number of columns (no ONS codes)
-clean_ctr_2("ctr_2021", ctr_2021_raw, "1920", "2021")
+clean_ctr_2("ctr_2021", ctr_2021_raw, "2021")
 
-clean_ctr_2("ctr_1920", ctr_1920_raw, "1819", "1920")
+clean_ctr_2("ctr_1920", ctr_1920_raw, "1920")
 
-clean_ctr_2("ctr_1819", ctr_1819_raw, "1718", "1819")
+clean_ctr_2("ctr_1819", ctr_1819_raw, "1819")
 
-clean_ctr_2("ctr_1718", ctr_1718_raw, "1617", "1718")
+clean_ctr_2("ctr_1718", ctr_1718_raw, "1718")
 
-clean_ctr_2("ctr_1617", ctr_1617_raw, "1516", "1617")
+clean_ctr_2("ctr_1617", ctr_1617_raw, "1617")
 
 #call third function due to changes in column names and number of columns
-clean_ctr_3("ctr_1516", ctr_1516_raw, "1415", "1516")
+clean_ctr_3("ctr_1516", ctr_1516_raw, "1516")
 
 view(ctr_2223)
 view(ctr_2122)
